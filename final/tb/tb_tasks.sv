60f32aae04fe51b8dda2e9cb9bfed14f4ba2b170
Joe Kardia <jdkardia@gmail.com>
2017-12-10 13:55:36 -0600

    Changed tb_tasks to the correct one

--- final/tb/tb_tasks.sv
+++ final/tb/tb_tasks.sv
@@ -1,28 +1,34 @@
-task send_cmd_task(input reg [7:0] cmd_num,
-                   output reg send_cmd,
-                   output reg cmd_to_copter);
+task automatic send_cmd_task(ref reg clk,
+                   input reg [7:0] cmd_num,
+                   ref reg send_cmd,
+                   ref reg [7:0] cmd_to_copter);
     begin
         cmd_to_copter = cmd_num;
+        @(negedge clk)
         send_cmd = 1;
+        @(negedge clk)
+        send_cmd = 0;
     end
 endtask
-
-task check_response_task(input reg resp_rdy);
+//not working if resp_rdy is a wire? don't use right now.
+task automatic check_response_task(ref logic resp_rdy);
     begin
-    fork : chk
-        begin
-            // Timeout check
-            #1000000
-            $display("%t : timeout", $time);
-            $stop;
-            disable chk;
-        end
-        begin
-            // Wait on signal
-            @(posedge resp_rdy);
-            disable chk;
-        end
-    join
+    //wait for response
+        fork : chk
+            begin
+                // Timeout check
+                #3000000
+                $display("%t : timeout", $time);
+                $stop;
+                disable chk;
+            end
+            begin
+                // Wait on signal
+                @(posedge resp_rdy);
+                $display("%t : resp received", $time);
+                disable chk;
+            end
+        join
     end
 endtask
 
@@ -31,19 +37,51 @@ task check_posack_task(input reg [7:0] resp);
         if(resp == 8'ha5)begin
             $display("PosAck Received.");
         end else begin
-            $display("PosAck not received.");
+            $display("BAD POSACK RECEIVED. FAILURE. %h",resp);
             $stop;
         end
     end
 endtask
 
-task check_batt_task(input reg [7:0] resp);
+task check_batt_task(input reg [7:0] resp, input reg [7:0] expected);
     begin
-        if(resp == 8'hc0)begin
+        if(resp == expected)begin
             $display("batt Received.");
         end else begin
-            $display("batt not received.");
+            $display("BAD BATT RECEIVED. FAILURE. %h",resp);
             $stop;
         end
     end
 endtask
+
+task automatic init_task(ref reg clk, ref reg RST_n, ref reg send_cmd);
+    begin
+        clk = 1;
+        @(posedge clk)RST_n = 0;
+        @(posedge clk)RST_n = 1;
+        send_cmd = 0;
+    end
+endtask
+
+task check_thrust_task(input reg [8:0] thrust, input reg [15:0] expected);
+begin
+        if(thrust == expected[8:0])begin
+            $display("thrust set correctly.");
+        end else begin
+            $display("BAD THRUST SET. FAILURE. %h",thrust);
+            $stop;
+        end
+    end
+endtask
+
+task check_pry_task(input reg [15:0] pry , input reg [15:0] expected);
+begin
+        if(pry == expected)begin
+            $display("value set correctly.");
+        end else begin
+            $display("BAD VALUE SET. FAILURE. %h. expected = %h",pry, expected);
+            $stop;
+        end
+    end
+endtask
+
